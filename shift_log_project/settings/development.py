50a8240e5357a9be5c0742946d36c3993a21b7ba
EN <novichenkoee@kuban-vino.ru>
2025-11-10 17:40:19 +0300

    fix: исправлена проблема с debug_toolbar в development settings

--- shift_log_project/settings/development.py
+++ shift_log_project/settings/development.py
@@ -9,8 +9,14 @@ DEBUG = True
 ALLOWED_HOSTS = ["*"]
 
 # Development-specific settings
-if DEBUG:
-    INSTALLED_APPS += ['debug_toolbar']
-    MIDDLEWARE = ['debug_toolbar.middleware.DebugToolbarMiddleware'] + MIDDLEWARE
-    INTERNAL_IPS = ['127.0.0.1', 'localhost']
+# debug_toolbar опционален (установите django-debug-toolbar если нужен)
+try:
+    import debug_toolbar
+    if DEBUG:
+        INSTALLED_APPS += ['debug_toolbar']
+        MIDDLEWARE = ['debug_toolbar.middleware.DebugToolbarMiddleware'] + MIDDLEWARE
+        INTERNAL_IPS = ['127.0.0.1', 'localhost']
+except ImportError:
+    # debug_toolbar не установлен, пропускаем
+    pass
 
